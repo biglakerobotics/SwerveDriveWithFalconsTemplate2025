3b4548b3b8196282be714b630ef5a475eb50198a
Big Lake Robotics <biglakerobotics@gmail.com>
2025-01-23 16:18:57 -0600

    Added constants for PID and constants for KS for steer. Updated constant values for PID and KS

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,13 @@
 package frc.robot;
 
 public class Constants {
+//  Sets the slew rate limit
     public static final int SLEWRATELIMITER = 3;
-
+//  Sets P and D values of the PIDs 
+    public static final double DRIVE_P_VALUE = 0.2; 
+    public static final double DRIVE_D_VALUE = 0; 
+    public static final double STEER_P_VALUE = 20.1863; 
+    public static final double STEER_D_VALUE = 0.2; 
+//  Sets the KS value
+    public static final double STEER_S_VALUE = 0.15783;
 }
