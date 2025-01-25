d961cd640db2d69ca18c0229508987551ae30d62
Big Lake Robotics <biglakerobotics@gmail.com>
2025-01-25 16:27:10 -0600

    Rotational PID added

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
 //  Sets P and D values of the PIDs 
     public static final double DRIVE_P_VALUE = 0.2; 
     public static final double DRIVE_D_VALUE = 0; 
-    public static final double STEER_P_VALUE = 20.1863; 
-    public static final double STEER_D_VALUE = 0.2; 
+    public static final double STEER_P_VALUE = 48.076; 
+    public static final double STEER_D_VALUE = 1.3786; 
 //  Sets the KS value
     public static final double STEER_S_VALUE = 0.15783;
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.SignalLogger;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -15,6 +17,11 @@ public class Robot extends TimedRobot {
 
   public Robot() {
     m_robotContainer = new RobotContainer();
+  }
+
+  @Override
+  public void robotInit() {
+    SignalLogger.start();
   }
 
   @Override
