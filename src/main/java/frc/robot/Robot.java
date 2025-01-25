78dd61aca35776d3c40e6f5cb58012ec768bb97d
Big Lake Robotics <biglakerobotics@gmail.com>
2025-01-25 15:25:03 -0600

    photonvision works!

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,15 +4,24 @@
 
 package frc.robot;
 
+import org.photonvision.PhotonCamera;
+
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
+  private final PhotonCamera photonCamera = new PhotonCamera("dumbdumbcamera");
   private final RobotContainer m_robotContainer;
-
+  double yawData = 0;
+  double skewData = 0;
+  double pitchData = 0;
+  double areaData = 0;
+  Transform3d camToTarget;
   public Robot() {
     m_robotContainer = new RobotContainer();
   }
@@ -20,6 +29,33 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run(); 
+    var result = photonCamera.getLatestResult();
+    boolean hasTargets = result.hasTargets();
+    if (hasTargets) {
+      var photonTarget = result.getBestTarget();
+      yawData = photonTarget.getYaw();
+      skewData = photonTarget.getSkew();
+      pitchData = photonTarget.getPitch();
+      camToTarget = photonTarget.getBestCameraToTarget();
+      areaData = photonTarget.getArea();
+
+      // SmartDashboard.putBoolean("works???maybe??", true);
+      // System.out.println("WORKING ITS BUTT OFF");
+    }
+    else{
+      yawData = 0;
+      skewData = 0;
+      pitchData = 0;
+      areaData = 0;
+
+    }
+    SmartDashboard.putNumber("yawData", yawData);
+    SmartDashboard.putNumber("pitchData", pitchData);
+    SmartDashboard.putNumber("skewData", skewData);
+    SmartDashboard.putData("camtotarget", camToTarget);
+    SmartDashboard.putNumber("targetArea", areaData);
+
+
   }
 
   @Override
